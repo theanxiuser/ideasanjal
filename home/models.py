c28ed7cb95b3870fa10bfcdb9fe6de134efde0d6
theanxiuser <bishalpoudel607@gmail.com>
2022-07-14 19:04:47 +0545

    IsUser inherit from User model

--- home/models.py
+++ home/models.py
@@ -11,13 +11,13 @@ GENDER = (
 )
 
 
-class IsUser (models.Model):
+class IsUser (User):
     """
-    ISUser model uses User model. More fields are added,
+    ISUser model inherits User model. More fields are added,
     it is used as client login for first login part.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age = models.PositiveSmallIntegerField(null=False)
+
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
     gender = models.IntegerField(GENDER, default=3)
     profession = models.CharField(max_length=30, blank=True)
     bio = models.TextField(max_length=500, blank=True)
@@ -27,4 +27,9 @@ class IsUser (models.Model):
     twitter = models.URLField(null=True, blank=True)
     facebook = models.URLField(null=True, blank=True)
     github = models.URLField(null=True, blank=True)
-    
+
+    # https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#django.contrib.auth.models.CustomUser.REQUIRED_FIELDS
+    REQUIRED_FIELDS = ["first_name", "last_name", "username", "email", "password1", "password2"]
+
+    def __str__(self):
+        return self.username
