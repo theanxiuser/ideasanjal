9951b18724c031e9fe4c528827b09b2d9aee629b
theanxiuser <bishalpoudel607@gmail.com>
2022-07-14 10:05:10 +0545

    dir for media and staticfiles

--- ideasanjal/settings.py
+++ ideasanjal/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
