232bd8e868ed91f8593ca9557bf7e73bc20a3a18
theanxiuser <bishalpoudel607@gmail.com>
2022-07-15 22:38:13 +0545

    urlpatterns for login and logout

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . views import HomeView, register
+from . import views
 
 # including urlpatterns
 
 app_name = "home"
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("register/", register, name="register"),
+    path("", views.HomeView.as_view(), name="home"),
+
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
